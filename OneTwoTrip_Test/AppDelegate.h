c63b8edb2b01abd86d23ae6fbb1e48955cc80441
Kotov Max <maxpilgrim94@gmail.com>
2015-06-28 12:08:52 +0600

    Initial Commit

---
+++ OneTwoTrip_Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneTwoTrip_Test
+//
+//  Created by Kotov Max on 28/06/15.
+//  Copyright (c) 2015 kotovmd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OneTwoTrip_Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OneTwoTrip_Test
+//
+//  Created by Kotov Max on 28/06/15.
+//  Copyright (c) 2015 kotovmd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
